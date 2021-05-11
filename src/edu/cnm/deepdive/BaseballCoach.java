0f0a037a2d540768777e16c19f4ef8ada86c1aac
Jan Gevaert <jan.gevaert@gmail.com>
2021-05-11 16:35:26 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/BaseballCoach.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive;
+
+public class BaseballCoach  implements Coach{
+
+  @Override
+  public String getDailyWorkout() {
+    return "Spend 30 minutes on batting practice";
+  }
+
+}
---
+++ src/edu/cnm/deepdive/TrackCoach.java
@@ -0,0 +1,9 @@
+package edu.cnm.deepdive;
+
+public class TrackCoach implements Coach{
+
+  @Override
+  public String getDailyWorkout() {
+    return "Run a hard 5 k";
+  }
+}
