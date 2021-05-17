f399b59851dd6b2f672566fd213d48c8eb53fc43
Jan Gevaert <jan.gevaert@gmail.com>
2021-05-17 15:32:33 -0600

    Worked with singleton and prototype scope

---
+++ src/edu/cnm/deepdive/BeanScopeDemoApp.java
@@ -0,0 +1,30 @@
+package edu.cnm.deepdive;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+  public static void main(String[] args) {
+
+    //load the spring configuration file
+    ClassPathXmlApplicationContext context =
+        new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+    //retrieve the bean from the spring container
+    Coach theCoach = context.getBean("myCoach", Coach.class);
+    Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+    //check if these are the same beans
+    boolean result = (theCoach == alphaCoach);
+
+    //print out the results
+    System.out.println("\nPrinting to the same object: " + result);
+
+    System.out.println("\nMemory location for theCoach: " + theCoach);
+
+    System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+
+    //close the context
+    context.close();
+
+  }
+}
